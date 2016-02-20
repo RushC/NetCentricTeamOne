b058edb8119c1c893b3cd2e85a9c14b668d81dc7
MattD94 <MattD94@users.noreply.github.com>
2016-02-19 21:57:51 -0500

    added server side JSONP for quiz navigation

--- EvalJSONP/EvalJSONP.js
+++ EvalJSONP/EvalJSONP.js
@@ -21,24 +21,76 @@ function gettool(req, res) {
 				else
 					console.log("Sent " + filename);
 			});
+			break;
 	}
 }
 
 function posttool(req, res) {
+
 	switch (req.path) {
-		case "/EvalJSONP/getID":
-			res.jsonp(JSON.stringify(addUser())); //add the user and send them their ID
+		case "/EvalJSONP/first":
+			updateUser(req.query.userID, req.query.answer, "first");
+			sendQuestion(userID, res);
+			break;
+		case "/EvalJSONP/next":
+			updateUser(req.query.userID, req.query.answer, "next");
+			sendQuestion(userID, res);
+			break;
+		case "/EvalJSONP/previous":
+			updateUser(req.query.userID, req.query.answer, "previous");
+			sendQuestion(userID, res);
+			break;
+		case "/EvalJSONP/last":
+			updateUser(req.query.userID, req.query.answer, "last");
+			sendQuestion(userID, res);
+			break;
+		default:
+			console.log("Some noob can't use the internets lol");
+			res.status(400).end();
 	}
 };
 
-function sendQuestion(res, id, old) {}
+function updateUser(userID, answer) {
+	//first save the answer for the current question:
+	if (answer) {
+		users[userID].answers[users[userID].currentQuestion - 1] = answer;
+	}
+	//now set the current question based on the direction:
+	switch (direction) {
+		case "first":
+			users[userID].currentQuestion = 1;
+			break;
+		case "next":
+			++users[userID].currentQuestion;
+			break;
+		case "previous":
+			--users[userID].currentQuestion;
+			break;
+		case "last":
+			users[userID].currentQuestion = questions.length;
+			break;
+		default:
+			console.log("WTF>>>\n\n\n\n\n\n\n\n\n\n\n<<<SOME KIND OF SHIT WENT DOWN HERE");
+	}
 
+}
+
+function sendQuestion(userID, res) {
+	//create the send obj with the question and the users answer (if there is one):
+	var sendObj = {};
+	sendObj.question = questions[users[userID].currentQuestion - 1];
+	sendObj.currentAnswer = users[userID].answers[users[userID].currentQuestion - 1];
+	sendObj.questionNumber = users[userID].currentQuestion;
+	sendObj.questionTotal = questions.length;
+	//send the JSON notation for the sendObj as a response:
+	res.jsonp(JSON.stringify(sendObj));	
+}
 
 
 //constructor for user object:
 function User() {
 	this.currentQuestion = 1; //question of the quiz the user is viewing
-	this.amswers = []; //answers to the questions submitted by the user
+	this.answers = []; //answers to the questions submitted by the user
 }
 
 
