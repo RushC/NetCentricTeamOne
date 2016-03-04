2afc9f010856ce57f5d83d74b9833762f376e1e5
MattD94 <MattD94@users.noreply.github.com>
2016-03-04 18:18:25 -0500

    added flag class and skeleton drawFlag method

--- CanvasAnimation/canvasanimation.js
+++ CanvasAnimation/canvasanimation.js
@@ -1,3 +1,19 @@
+function Flag(x, y, width, height, image, startRotation) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.image = image;
+    this.rotation = startRotation % 360;
+}
+
+function drawFlag(context, flag) {
+    //change the values as appropriate here:
+    
+    //draw the image:
+    context.drawImage(flag.image, flag.x, flag.y, flag.width, flag.height);
+}
+
 addEventListener("load", function() {
     
     
