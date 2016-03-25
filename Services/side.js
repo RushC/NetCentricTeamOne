5231459d84bd38050f9c9eb347f25b9274c1c72e
NICHOLAS TOTOLOS <ntotolos@gmail.com>
2016-03-25 18:17:40 -0400

    Indexed the partial animation feed to synthesize user empathy towards the button matrices

--- Services/side.js
+++ Services/side.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
-   // Animate the side div in.
-    $("#sideDiv").hide().fadeIn();
+	// Animate the side div in.
+	$("#sideDiv").hide().fadeIn();
+	
+	// Add hover functionality to each button.
+	var buttons = $("button");
+	for (var i = 0; i < buttons.length; i++) {
+		// Add the hover class when the mouse enters the button.
+		$(buttons[i]).mouseenter(function() {
+			$(this).switchClass("", "hover", 200);
+		});
+		// Remove the hover class when the mouse leaves the button.
+		$(buttons[i]).mouseleave(function() {
+			$(this).switchClass("hover", "", 200);
+		});
+	}
 });
+
+function rosterJSP()
+{
+	parent.document.getElementById("cframe").contentWindow.location = "http://localhost:8888/WebRoster/roster.jsp"
+}
