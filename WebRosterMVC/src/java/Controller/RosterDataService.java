705603de0a12c4f11a76783ccbdbf6f57316dbc0
MattD94 <MattD94@users.noreply.github.com>
2016-04-05 17:07:15 -0400

    implemented the load roster method and cleaned up the Roster bean a bit

--- WebRosterMVC/src/java/Controller/RosterDataService.java
+++ WebRosterMVC/src/java/Controller/RosterDataService.java
@@ -7,6 +7,8 @@ package Controller;
  */
 
 
+import Model.Roster;
+import Model.Student;
 import java.io.File;
 import java.util.ArrayList;
 
@@ -15,16 +17,52 @@ import java.util.ArrayList;
  * @author mwd5503
  */
 public class RosterDataService {
-    private final ArrayList<Student>    students;
     
-    public RosterDataService() {
-        students = new ArrayList<>();
-    }
-    
-    public Model.Roster loadStudents(File f) throws java.io.IOException {
-        students.clear();
+    public Roster loadRoster(File f) throws java.io.IOException {
+        
         java.io.BufferedReader in = new java.io.BufferedReader(new java.io.FileReader(f));
         String line = in.readLine();
-        return null;
+        ArrayList<Student> students = new ArrayList<>();
+        while (line != null) {
+            // Create a new student bean:
+            Student studentBean = new Student();
+
+                // Set the last name.
+                studentBean.setLastName(line.trim());
+                
+                // Get the first name.
+                line = in.readLine();
+                line = line == null ? "" : line;
+                
+                // Set the first name.
+                studentBean.setFirstName(line.trim());
+                
+                // Get the ID.
+                line = in.readLine();
+                line = line == null ? "" : line;
+                
+                // Set the ID:
+                studentBean.setID(line.trim());
+                
+                // Get the Team number.
+                line = in.readLine();
+                line = line == null ? "" : line;
+                
+                // Set the team number.
+                studentBean.setTeamNumber(line.trim());
+                
+                // Add the student to the array list.
+                students.add(studentBean);
+            }
+        
+            // Set up a roster bean with the students array.
+            Roster rosterBean = new Roster();
+            rosterBean.putStudents(students.toArray(new Student[0]));
+            
+            // Close the input stream.
+            in.close();
+            
+            // Return the roster bean.
+            return rosterBean;
     }
 }
--- WebRosterMVC/src/java/Model/Roster.java
+++ WebRosterMVC/src/java/Model/Roster.java
@@ -18,6 +18,10 @@ import javax.servlet.ServletContext;
 public class Roster implements java.io.Serializable {
     private Student[]       students;
     
+    
+    public void putStudents(Student[] students) {
+        this.students = students;
+    }
     /**
      * Accessor method.
      */
@@ -36,57 +40,5 @@ public class Roster implements java.io.Serializable {
             e.printStackTrace();
             return "";
         }        
-    }
-    
-    /**
-     * Loads the student data from the specified file path which is loaded
-     * using the specified servlet context.
-     * 
-     * @param context the servlet context (typically the application)
-     * @param filePath the path to the resource file to read all the student
-     *                 roster information from.
-     */
-    public void loadData(ServletContext context, String filePath) {
-        // Attempt to load and read the given file.
-        try (Scanner in = new Scanner(context.getResourceAsStream(filePath))) {
-            // Start building a list of Students.
-            ArrayList<Student> students = new ArrayList<>();
-            
-            // Continue reading through the resource until the end of the
-            // file is encountered.
-            while (in.hasNext()) {
-                // Retrieve the last name.
-                String lastName = in.next();
-                
-                // Ensure there is another word.
-                if (!in.hasNext())
-                    break;
-                // Retrieve the first name.
-                String firstName = in.next();
-                
-                // Retrieve the id.
-                if (!in.hasNext())
-                    break;
-                String id = in.next();
-                
-                // Retrieve the team number.
-                if (!in.hasNext())
-                    break;
-                String teamNumber = in.next();
-                
-                // Create the student from the retrieved information.
-                Student student = new Student();
-                student.setFirstName(firstName);
-                student.setLastName(lastName);
-                student.setID(id);
-                student.setTeamNumber(teamNumber);
-                
-                // Add the student to the list.
-                students.add(student);
-            }
-            
-            // Set the students array.
-            this.students = students.toArray(new Student[0]);
-        }
     }
 }
