070beca906b5a5806ad4b78917c77410efc25bf8
NICHOLAS TOTOLOS <ntotolos@gmail.com>
2016-04-15 19:25:42 -0400

    Controller setup

--- ShowAndTellProject/src/java/Controller/Controller.java
+++ ShowAndTellProject/src/java/Controller/Controller.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author njt5112
  */
-public class Controller extends HttpServlet{
+public class Controller extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
@@ -26,6 +26,44 @@ public class Controller extends HttpServlet{
     protected void doPost(HttpServletRequest request,
             HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
-        
+        if(action != null){
+            /*
+            actions:
+                newLecture
+                newPage
+                newEntity
+                deleteEntity
+                deletePage
+                deleteLecture
+                changeEntity
+                changePage
+                changeLecture
+                getLecture
+                getLectureList
+            */
+            if(action.equals("newLecture")){
+                
+            }else if(action.equals("newPage")){
+                
+            }else if(action.equals("newEntity")){
+                
+            }else if(action.equals("deleteEntity")){
+                
+            }else if(action.equals("deletePage")){
+                
+            }else if(action.equals("deleteLecture")){
+                
+            }else if(action.equals("changeEntity")){
+                
+            }else if(action.equals("changePage")){
+                
+            }else if(action.equals("changeLecture")){
+                
+            }else if(action.equals("getLecture")){
+                
+            }else if(action.equals("getLectureList")){
+                
+            }
+        }
     }
 }
