265c65d4a16ebf79962ef956e392f0cc5f7cbc8d
MattD94 <MattD94@users.noreply.github.com>
2016-04-15 15:13:19 -0400

    progress on client side of editor

---
+++ ShowAndTell/ShowAndTell.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="ShowAndTell.css"></link>
+        <!-- jQuery and jQueryUI used for animations. -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+        <script src="lecturenotes.js"></script>
+    </head>
+    <body>
+		<div id="notesDiv"></div>
+		<div id="textDiv">
+			<form>
+			</form>
+		</div>
+    </body>
+</html>
