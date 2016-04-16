97a8b1c6a110c25f428587c012093bcc8f205ce5
MattD94 <MattD94@users.noreply.github.com>
2016-04-15 22:07:30 -0400

    more progress on client. DONT TOUCH MY CODE NICK.

--- ShowAndTell/ShowAndTell.js
+++ ShowAndTell/ShowAndTell.js
@@ -107,11 +107,66 @@ function changeType() {
 
 
 function updatePropertyDiv() {
-    if (currentEntity) {
+    if (currentEntity) { //if the current entity is defined
+        // Set all the properties equal to those of the entity:
         xInput.val(currentEntity.x);
         yInput.val(currentEntity.y);
         zInput.val(currentEnttiy.z);
+<<<<<<< Updated upstream
         //switch()
+=======
+        // fill the content div with the appropriate elements based on the entity type:
+        entityContent.empty();
+        switch(currentEntity.type) {
+            case "image" : // create an image preview and a image loader if the entity is an image
+                // set the type input first:
+                typeInput[0].selectedIndex = 3;
+                // create and add the components for the content div:
+                var fileButton = $('<input type="file">');
+                var image = $('<img>');
+                image.hide(0);
+                fileButton[0].onchange = function () {
+                    var reader = new FileReader();
+                    var file = $("input[type=file]")[0].files[0];
+                    if (file) {
+                        reader.onloadend = function() {
+                            image[0].src = reader.result;
+                            currentEntity.content = reader.result;
+                            image[0].width = 25;
+                            image[0].height = 25;
+                            image[0].src = reader.result;
+                        };
+
+                        reader.readAsDataURL(file);
+                    }
+                };
+                entityContent.append(fileButton);
+                entityContent.append(image);
+                entityContent.show();
+                break;
+                
+            // create a basic text box if the input is a text box or list:
+            case "textbox" :
+            case "bulletlist" :
+                // set the type input selector first:
+                if(currentContext.type == "textbox")
+                    typeInput[0].selectedIndex = 1;
+                else
+                    typeInput[0].selectedIndex = 2;
+                var textContent = $('<textarea id="textContent"></textarea>');
+                textContent[0].width = currentEntity.width;
+                textContent[0].height = currentEntity.height;
+                textContent[0].innerHTML = currentEntity.content;
+                entityContent.append(textContent);
+                textContent.onChange = updateEntityElementContent;
+                break;
+            default :
+                Console.log("Bad Entity Type" + currentEntity.type);
+                break;
+        }
+        updateEntityElementContent();
+    
+>>>>>>> Stashed changes
     } else {
         xInput.val(0);
         yInput.val(0);
@@ -136,6 +191,58 @@ $(document).ready(function() {
 		});
 	}
 });
+
+
+function updateEntityElementContent() {
+        if (currentEntity) {
+        //first get the element
+        var element = $("#" + currentEntity.id);
+        // create the element if it doesn't exist:
+        if (!element[0]) {
+            element = $('<div id="' + currentEntity.id +'"></div>');
+            element.css({position: "absolute",
+                        left: currentEntity.x,
+                        top: currentEntity.y,
+                        "z-index": currentEntity.z,
+                        size:  "auto"});
+            // add the element to the SHIT FUCKING TITS
+            slideDiv.add(element);
+        }
+        // add/update the appropriate contents
+        switch(currentEntity.type) {
+            case "image" :
+                // get the element for the entity:
+                var image = element.children("img");
+                //empty the div and create a new image element if one doesn't exist"
+                if(!image[0]) {
+                    image = $("<img>");
+                    element.empty();
+                    elememt.append(image);
+                }
+                // set the image's width, height, and source:
+                image.width = currentEntity.width;
+                image.height = currentEntity.height;
+                image.src = currentEntity.content;
+                break;
+            case "textbox" :
+                //get the element for the entity:
+                var box = element.children("textarea");
+                //empty the div and create a new textarea if one doesn't exist:
+                if (!box[0]) {
+                    box = $('<textarea></textarea>');
+                    box[0].wrap = true;
+                    box[0].readOnly = true;
+                    element.empty();
+                    element.append(box);
+                }
+                // set the content of the text area to that of the 
+                box.val(currentEntity.content);
+                box[0].rows = currentEntity.width;
+                box[0].cols = currentEntity.height;
+                
+        }
+    }
+}
 
 
 
