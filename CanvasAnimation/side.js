6cf47859c27764f845c2883df4728ea22b384a18
NICHOLAS TOTOLOS <ntotolos@gmail.com>
2016-04-20 16:52:28 -0400

    Small stylizing

--- CanvasAnimation/side.js
+++ CanvasAnimation/side.js
@@ -27,4 +27,17 @@ addEventListener("load", function() {
         // Recreate the flags with the specified settings.
         parent.document.getElementById("cframe").contentWindow.makeFlags(rows, flagsPerRow, 0.5, maxSpinRate, 1);  
     };
+	
+	// Add hover functionality to each button.
+	var buttons = $("button");
+	for (var i = 0; i < buttons.length; i++) {
+		// Add the hover class when the mouse enters the button.
+		$(buttons[i]).mouseenter(function() {
+			$(this).switchClass("", "hover", 200);
+		});
+		// Remove the hover class when the mouse leaves the button.
+		$(buttons[i]).mouseleave(function() {
+			$(this).switchClass("hover", "", 200);
+		});
+	}
 });
